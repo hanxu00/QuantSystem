fa018546b17644edc58542897270d9ed33c501ee
hanxu00 <hanxv784158666@163.com>
2020-07-10 23:55:54 +0100

    1

--- Dissertation/MomentumStrategy_Dissertation.py
+++ Dissertation/MomentumStrategy_Dissertation.py
@@ -107,32 +107,41 @@ umd.head()
 # For each date: assign ranking 1-10 based on cumret
 # 1=lowest 10=highest cumret
 umd=umd.dropna(axis=0, subset=['cumret'])
-umd.head()
+
 #transform函数后面再学一下
 umd['momr']=umd.groupby('date')['cumret'].transform(lambda x: pd.qcut(x, 10, labels=False))
 
 umd.momr=umd.momr.astype(int)
 umd['momr'] = umd['momr']+1
-#####################进度mark，预计剩余时间2h######################
+umd.head()
+
 # Corrected previous version month end line up issue
 # First lineup date to month end date medate
 # Then calculate hdate1 and hdate2 using medate
 
+# MonthEnd MonthBegin是日期便偏移函数，详细的看一下pandas时间序列相关内容
+# https://www.pypandas.cn/docs/user_guide/timeseries.html#日期-时间组件
 umd['form_date'] = umd['date']
 umd['medate'] = umd['date']+MonthEnd(0)
 umd['hdate1']=umd['medate']+MonthBegin(1)
 umd['hdate2']=umd['medate']+MonthEnd(K)
 
 umd = umd[['permno', 'form_date','momr','hdate1','hdate2']]
-
+umd.head()
 # join rank and return data together
 # note: this step consumes a lot of memory so takes a while
 _tmp_ret = crsp_m[['permno','date','ret']]
 port = pd.merge(_tmp_ret, umd, on=['permno'], how='inner')
+# merge是按permno来的，所以_tmp_ret里每个permno和date后面都跟了umd里每个permno里的数据，按下面这个规则可以剔除
 port = port[(port['hdate1']<=port['date']) & (port['date']<=port['hdate2'])]
 
 umd2 = port.sort_values(by=['date','momr','form_date','permno']).drop_duplicates()
 umd3 = umd2.groupby(['date','momr','form_date'])['ret'].mean().reset_index()
+
+umd2.head()
+umd2.shape
+umd3.head()
+umd3.shape
 
 # Skip first two years of the sample 
 start_yr = umd3['date'].dt.year.min()+2
@@ -144,9 +153,14 @@ ewret = umd3.groupby(['date','momr'])['ret'].mean().reset_index()
 ewstd = umd3.groupby(['date','momr'])['ret'].std().reset_index()
 ewret = ewret.rename(columns={'ret':'ewret'})
 ewstd = ewstd.rename(columns={'ret':'ewretstd'})
+ewret.head()
+ewret.shape
+ewstd.head()
+ewstd.shape
 ewretdat = pd.merge(ewret, ewstd, on=['date','momr'], how='inner')
 ewretdat = ewretdat.sort_values(by=['momr'])
-
+ewretdat.head()
+ewretdat.shape
 # portfolio summary
 ewretdat.groupby(['momr'])['ewret'].describe()[['count','mean', 'std']]
 
@@ -155,23 +169,24 @@ ewretdat.groupby(['momr'])['ewret'].describe()[['count','mean', 'std']]
 #################################
 
 # Transpose portfolio layout to have columns as portfolio returns
+# pivot这个好用！:Return reshaped DataFrame organized by given index / column values.
 ewretdat2 = ewretdat.pivot(index='date', columns='momr', values='ewret')
-
-# Add prefix port in front of each column
+ewretdat2.head()
+# Add prefix port in front of each column. prefix:批量重命名
 ewretdat2 = ewretdat2.add_prefix('port')
 ewretdat2 = ewretdat2.rename(columns={'port1':'losers', 'port10':'winners'})
 ewretdat2['long_short'] = ewretdat2['winners'] - ewretdat2['losers']
-
+ewretdat2.head()
 # Compute Long-Short Portfolio Cumulative Returns
 ewretdat3 = ewretdat2
 ewretdat3['1+losers']=1+ewretdat3['losers']
 ewretdat3['1+winners']=1+ewretdat3['winners']
 ewretdat3['1+ls'] = 1+ewretdat3['long_short']
-
+#cumprod一个连乘函数，和prod有啥区别？
 ewretdat3['cumret_winners']=ewretdat3['1+winners'].cumprod()-1
 ewretdat3['cumret_losers']=ewretdat3['1+losers'].cumprod()-1
 ewretdat3['cumret_long_short']=ewretdat3['1+ls'].cumprod()-1
-
+ewretdat3.head()
 #################################
 # Portfolio Summary Statistics  #
 ################################# 
@@ -179,8 +194,9 @@ ewretdat3['cumret_long_short']=ewretdat3['1+ls'].cumprod()-1
 # Mean 
 mom_mean = ewretdat3[['winners', 'losers', 'long_short']].mean().to_frame()
 mom_mean = mom_mean.rename(columns={0:'mean'}).reset_index()
-
+mom_mean
 # T-Value and P-Value
+# DataFrame.T: Transpose index and columns.
 t_losers = pd.Series(stats.ttest_1samp(ewretdat3['losers'],0.0)).to_frame().T
 t_winners = pd.Series(stats.ttest_1samp(ewretdat3['winners'],0.0)).to_frame().T
 t_long_short = pd.Series(stats.ttest_1samp(ewretdat3['long_short'],0.0)).to_frame().T
@@ -191,6 +207,8 @@ t_long_short['momr']='long_short'
 
 t_output =pd.concat([t_winners, t_losers, t_long_short])\
     .rename(columns={0:'t-stat', 1:'p-value'})
-
+t_output
 # Combine mean, t and p
 mom_output = pd.merge(mom_mean, t_output, on=['momr'], how='inner')
+mom_output
+conn.close()
